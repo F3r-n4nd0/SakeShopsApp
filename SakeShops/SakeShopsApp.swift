54250459b072ecaf7dec89556da87cdc241ecbba
Fernando Luna <fluna.mol@gmail.com>
2026-05-29 08:54:26 -0400

    Initial Commit

---
+++ SakeShops/SakeShopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SakeShopsApp.swift
+//  SakeShops
+//
+//  Created by Fernando Luna on 29/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SakeShopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SakeShopsTests/SakeShopsTests.swift
@@ -0,0 +1,19 @@
+//
+//  SakeShopsTests.swift
+//  SakeShopsTests
+//
+//  Created by Fernando Luna on 29/5/26.
+//
+
+import Testing
+@testable import SakeShops
+
+struct SakeShopsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
